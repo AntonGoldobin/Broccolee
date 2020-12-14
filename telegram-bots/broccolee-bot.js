f24273c8599e9b6b8ee207054699bc494f91213b
Goldobin Anton <722451@it-atlantic.com>
2020-12-15 02:45:35 +0300

    new schedule

--- telegram-bots/broccolee-bot.js
+++ telegram-bots/broccolee-bot.js
@@ -1,5 +1,6 @@
 const Telegraf = require("telegraf");
 const schedule = require("node-schedule");
+var cron = require("node-cron");
 const snoowrap = require("snoowrap");
 const dotenv = require("dotenv");
 
@@ -7,6 +8,9 @@ dotenv.config();
 
 const testChannelId = "@broccoleeBoobs";
 const token = process.env.BROCCOLEE_BOT_TOKEN;
+
+const postingDelayMin = 10;
+const jobReplyConfig = "* */12 * * *";
 
 const r = new snoowrap({
   userAgent:
@@ -16,78 +20,82 @@ const r = new snoowrap({
   refreshToken: process.env.BROCCOLEE_SNOOWRAP_REFRESH_TOKEN,
 });
 
+let job = null;
+
 function startBot() {
-  let job = null;
+
   const testBot = new Telegraf(token);
   testBot.command("start", (ctx) => {
     ctx.reply("Очередь запущена!");
-    let startTime = new Date(Date.now() + 5000);
 
-    console.log("The schedule was restarted: " + getCurrentTime());
+    console.log("The schedule will be started soon: " + getCurrentTime());
+    getRedditPosts(ctx);
 
-    job = schedule.scheduleJob({ start: startTime, rule: "0 0 */12 * * *" }, function() {
-      getRedditPosts(ctx);
-    });
+    job = cron.schedule(
+      jobReplyConfig,
+      () => {
+        console.log("The schedule was started: " + getCurrentTime());
+        getRedditPosts(ctx);
+      }
+    );
+    job.start();
   });
 
   testBot.command("stop", (ctx) => {
     ctx.reply("Остановлено!");
     if (job) {
-      job.cancel();
+      console.log("The schedule was stoped: " + getCurrentTime());
+      job.destroy();
       job = null;
     }
   });
 
   testBot.launch();
-
-  const getRedditPosts = (ctx) => {
-    console.log("The schedule is started: " + getCurrentTime());
-
-    r
-      .getHot({ time: "day", limit: 100 })
-      .then((hotPosts) => sendPostsToChannel(hotPosts, ctx))
-      .catch((err) => console.log("Reddit error: " + getCurrentTime() + "\n" + err));
-  };
-
-  const sendPostsToChannel = (posts, ctx) => {
-    console.log(posts.length);
-    posts.some((post, i) => {
-      setTimeout(() => {
-        // if (!job) {
-        //   return true;
-        // }
-        if (post.url.includes("redgifs") || post.url.includes(".gifv")) {
-          ctx.telegram.sendAnimation(testChannelId, post.preview.reddit_video_preview.fallback_url, {
-            caption: `${post.title} \n [link](https://www.reddit.com/${post.permalink})`,
-            parse_mode: "MarkdownV2",
-          });
-        } else {
-          ctx.telegram.sendPhoto(testChannelId, post.url, {
-            caption: `${post.title} \n [link](https://www.reddit.com/${post.permalink})`,
-            parse_mode: "MarkdownV2",
-          });
-        }
-      }, 10 * 60 * 1000 * i);
-    });
-  };
-
-  const getCurrentTime = () => {
-    var currentdate = new Date();
-    return (
-      "Last Sync: " +
-      currentdate.getDate() +
-      "/" +
-      (currentdate.getMonth() + 1) +
-      "/" +
-      currentdate.getFullYear() +
-      " @ " +
-      currentdate.getHours() +
-      ":" +
-      currentdate.getMinutes() +
-      ":" +
-      currentdate.getSeconds()
-    );
-  };
 }
+
+const getRedditPosts = (ctx) => {
+  r.getHot({ time: "day", limit: 100 }).then((hotPosts) => sendPostsToChannel(hotPosts, ctx));
+};
+
+const sendPostsToChannel = (posts, ctx) => {
+  posts.some((post, i) => {
+    setTimeout(() => {
+      if (!job) {
+        return true;
+      }
+
+      const text = `${post.title} \n[link](https://www.reddit.com/${post.permalink})`;
+
+      if (post.url.includes("redgifs") || post.url.includes(".gifv")) {
+        ctx.telegram.sendAnimation(testChannelId, post.preview.reddit_video_preview.fallback_url, {
+          caption: text,
+          parse_mode: "Markdown",
+        });
+      } else {
+        ctx.telegram.sendPhoto(testChannelId, post.url, {
+          caption: text,
+          parse_mode: "Markdown",
+        });
+      }
+    }, postingDelayMin * 60 * 1000 * i);
+  });
+};
+
+const getCurrentTime = () => {
+  var currentdate = new Date();
+  return (
+    currentdate.getDate() +
+    "/" +
+    (currentdate.getMonth() + 1) +
+    "/" +
+    currentdate.getFullYear() +
+    " " +
+    currentdate.getHours() +
+    ":" +
+    currentdate.getMinutes() +
+    ":" +
+    currentdate.getSeconds()
+  );
+};
 
 module.exports.startBot = startBot;
