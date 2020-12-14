e06467bdebc25c3ac017560675d1e3ca854734ad
Goldobin Anton <722451@it-atlantic.com>
2020-12-14 21:35:45 +0300

    added time logs

--- telegram-bots/broccolee-bot.js
+++ telegram-bots/broccolee-bot.js
@@ -1,12 +1,30 @@
 const Telegraf = require("telegraf");
 const schedule = require("node-schedule");
 const snoowrap = require("snoowrap");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
 dotenv.config();
 
 const testChannelId = "@broccoleeBoobs";
 const token = process.env.BROCCOLEE_BOT_TOKEN;
+
+const getCurrentTime = () => {
+  var currentdate = new Date();
+  return (
+    "Last Sync: " +
+    currentdate.getDate() +
+    "/" +
+    (currentdate.getMonth() + 1) +
+    "/" +
+    currentdate.getFullYear() +
+    " @ " +
+    currentdate.getHours() +
+    ":" +
+    currentdate.getMinutes() +
+    ":" +
+    currentdate.getSeconds()
+  );
+};
 
 const r = new snoowrap({
   userAgent:
@@ -22,7 +40,9 @@ function startBot() {
   testBot.command("start", (ctx) => {
     ctx.reply("Очередь запущена!");
     let startTime = new Date(Date.now() + 5000);
-    job = schedule.scheduleJob({ start: startTime, rule: "0 0 */9 * * *" }, function(){
+    job = schedule.scheduleJob({ start: startTime, rule: "0 0 */9 * * *" }, function() {
+      console.log("Schedule is started: " + getCurrentTime());
+
       getRedditPosts(ctx);
     });
   });
@@ -38,7 +58,10 @@ function startBot() {
   testBot.launch();
 
   const getRedditPosts = (ctx) => {
-    r.getHot({ time: "day", limit: 100 }).then((hotPosts) => sendPostsToChannel(hotPosts, ctx));
+    r
+      .getHot({ time: "day", limit: 100 })
+      .then((hotPosts) => sendPostsToChannel(hotPosts, ctx))
+      .catch((err) => console.log("Reddit error: " + getCurrentTime() + "\n" + err));
   };
 
   const sendPostsToChannel = (posts, ctx) => {
@@ -49,9 +72,15 @@ function startBot() {
         //   return true;
         // }
         if (post.url.includes("redgifs") || post.url.includes(".gifv")) {
-          ctx.telegram.sendAnimation(testChannelId, post.preview.reddit_video_preview.fallback_url,  { caption: `${post.title} \n [link](https://www.reddit.com/${post.permalink})` , parse_mode: 'MarkdownV2' });
+          ctx.telegram.sendAnimation(testChannelId, post.preview.reddit_video_preview.fallback_url, {
+            caption: `${post.title} \n [link](https://www.reddit.com/${post.permalink})`,
+            parse_mode: "MarkdownV2",
+          });
         } else {
-          ctx.telegram.sendPhoto(testChannelId, post.url,  { caption: `${post.title} \n [link](https://www.reddit.com/${post.permalink})` , parse_mode: 'MarkdownV2' });
+          ctx.telegram.sendPhoto(testChannelId, post.url, {
+            caption: `${post.title} \n [link](https://www.reddit.com/${post.permalink})`,
+            parse_mode: "MarkdownV2",
+          });
         }
       }, 10 * 60 * 1000 * i);
     });
