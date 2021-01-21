a61a2887069ed2fbb6be14ff51b09cafc0b67c35
Goldobin Anton <722451@it-atlantic.com>
2021-01-22 02:17:58 +0300

    implemented own npm library

--- telegram-bots/universalBot/gettingRedgifsVideo.js
+++ telegram-bots/universalBot/gettingRedgifsVideo.js
@@ -1,33 +1,8 @@
-const webdriver = require("selenium-webdriver");
-const chrome = require("selenium-webdriver/chrome");
-require("chromedriver");
+const Redgifs = require("redgifs");
 
-let options = new chrome.Options();
-//Below arguments are critical for Heroku deployment
-options.addArguments("--no-sandbox");
-options.addArguments("--disable-infobars");
-options.addArguments("--disable-dev-shm-usage");
-options.addArguments("--headless");
-options.addArguments("--disable-gpu");
-
-const getRedgifsVideo = (url) => {
-	return new Promise(function(resolve, reject) {
-		let driver = new webdriver.Builder().forBrowser("chrome").setChromeOptions(options).build();
-
-		driver.get(url);
-		driver
-			.wait(webdriver.until.elementLocated(webdriver.By.css("video source:first-child")), 20000)
-			.then((el) => {
-				setTimeout(() => {
-					driver.quit();
-				}, 100);
-				resolve(el.getAttribute("src"));
-			})
-			.catch((err) => {
-				console.log("Selenium  BROCCOLEE error: " + err);
-				reject(err);
-			});
-	});
+const getRedgifsVideo = (pageUrl) => {
+	const r = new Redgifs();
+	return r.getRedgifsVideo(pageUrl);
 };
 
 module.exports.getRedgifsVideo = getRedgifsVideo;
